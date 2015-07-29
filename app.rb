f7f937f55853411dff16e0e461526db03d5866a6
Riaz N Virani <rvirani@emcien.com>
2015-07-29 15:14:08 -0400

    Added Contacts Page

--- app.rb
+++ app.rb
@@ -14,10 +14,10 @@ class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
 
-#get "/contacts" do
-#  @contacts = Contact.all
-#  erb :index
-#end
+get "/contacts" do
+ @contacts = Contact.all
+ erb :index
+end
 
 get "/create" do
   CREATE_URL = 'https://connect.heroku.com/dashboard-next/create-connection'
